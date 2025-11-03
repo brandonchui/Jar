6a485a73a7e87295fea584b4203da6e9fe650b31
brandonchui <chuib@usc.edu>
2025-11-02 20:30:01 -0800

    updated slang pipeline support for multi render targets
    for deferred renderer

--- src/graphics/slang/SlangPSO.h
+++ src/graphics/slang/SlangPSO.h
@@ -19,6 +19,12 @@ namespace SlangHelper
 							 DXGI_FORMAT renderTargetFormat = DXGI_FORMAT_R8G8B8A8_UNORM,
 							 DXGI_FORMAT depthStencilFormat = DXGI_FORMAT_D32_FLOAT);
 
+	/// Creates the PSO when you have multiple render targets
+	ID3D12PipelineState*
+	CreatePSOWithSlangShaderMRT(const CompiledShaderData& shaderData, ID3D12Device* device,
+								const DXGI_FORMAT* renderTargetFormats, uint32_t numRenderTargets,
+								DXGI_FORMAT depthStencilFormat = DXGI_FORMAT_D32_FLOAT);
+
 #endif // HAS_SLANG
 
 } // namespace SlangHelper
