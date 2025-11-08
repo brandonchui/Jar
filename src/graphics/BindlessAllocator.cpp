84e2d6551bbf80e802b2f59287dd38e47495f444
brandonchui <chuib@usc.edu>
2025-11-08 05:21:19 -0800

    free list impl

--- src/graphics/BindlessAllocator.cpp
+++ src/graphics/BindlessAllocator.cpp
@@ -1,6 +1,23 @@
 #include "BindlessAllocator.h"
 #include "Core.h"
 #include "directx/d3d12.h"
+#include <spdlog/sinks/stdout_color_sinks.h>
+
+std::shared_ptr<spdlog::logger> BindlessAllocator::sLogger = nullptr;
+
+void BindlessAllocator::InitLogger()
+{
+	if (!sLogger)
+	{
+		sLogger = spdlog::get("BindlessAllocator");
+		if (!sLogger)
+		{
+			sLogger = spdlog::stdout_color_mt("BindlessAllocator");
+			sLogger->set_pattern("[%H:%M:%S] [%^%l%$] [%n] %v");
+			sLogger->set_level(spdlog::level::debug);
+		}
+	}
+}
 
 bool BindlessAllocator::Initialize(uint32_t numDescriptors,
 								   D3D12_DESCRIPTOR_HEAP_TYPE descriptorType)
@@ -9,6 +26,7 @@ bool BindlessAllocator::Initialize(uint32_t numDescriptors,
 	{
 		return false;
 	}
+	InitLogger();
 
 	mDescriptorType = descriptorType;
 	if (mDescriptorType == D3D12_DESCRIPTOR_HEAP_TYPE_CBV_SRV_UAV)
@@ -43,18 +61,47 @@ bool BindlessAllocator::Initialize(uint32_t numDescriptors,
 
 Allocation BindlessAllocator::Allocate(uint32_t count)
 {
-	if (mNextFreeIndex + count > mHeapCount)
-	{
-		//TODO log
-		return {UINT32_MAX, UINT32_MAX};
-	}
 	Allocation res = {};
-	res.mCount = count;
-	res.mStartIndex = mNextFreeIndex;
 
-	mNextFreeIndex += count;
+	if (mFreeList.contains(count))
+	{
+		// Free list contains available _count_ of descriptors. Use the value pair to get start.
+		res.mCount = count;
+		res.mStartIndex = mFreeList.at(count).back();
+
+		mFreeList.at(count).pop_back();
+	}
+	else
+	{
+		if (mNextFreeIndex + count > mHeapCount)
+		{
+			sLogger->error("Out of bounds");
+			return {.mCount = UINT32_MAX, .mStartIndex = UINT32_MAX};
+		}
+
+		// Free list is empty OR no suitable allocation
+		// TODO should be implemented to find split blocks or something
+		res.mCount = count;
+		res.mStartIndex = mNextFreeIndex;
+
+		mNextFreeIndex += count;
+	}
 
 	return res;
+}
+
+bool BindlessAllocator::Free(Allocation allocation)
+{
+	if (allocation.mStartIndex > mHeapCount || !allocation.IsValid())
+	{
+		return false;
+	}
+
+	mFreeList[allocation.mCount].push_back(allocation.mStartIndex);
+
+	sLogger->info("Freed {} descriptors at index {}", allocation.mCount, allocation.mStartIndex);
+
+	return true;
 }
 
 DescriptorHandle BindlessAllocator::GetHandle(uint32_t index)
@@ -68,4 +115,13 @@ DescriptorHandle BindlessAllocator::GetHandle(uint32_t index)
 	gpuHandle.ptr = mGpuStart.ptr + (static_cast<size_t>(index) * mDescriptorSize);
 
 	return {cpuHandle, gpuHandle};
+}
+
+void BindlessAllocator::FreeDeferred(Allocation allocation, uint64_t fence)
+{
+	//TODO
+}
+void BindlessAllocator::ProcessDeletions(uint64_t completedFence)
+{
+	//TODO
 }
--- src/graphics/BindlessAllocator.h
+++ src/graphics/BindlessAllocator.h
@@ -1,9 +1,14 @@
 #pragma once
 
+#include <queue>
 #include <wrl.h>
 #include "DescriptorHeap.h"
 #include "directx/d3d12.h"
 #include <cstdint>
+#include <map>
+#include <vector>
+#include <memory>
+#include <spdlog/spdlog.h>
 
 struct Allocation
 {
@@ -11,6 +16,12 @@ struct Allocation
 	uint32_t mCount;
 
 	bool IsValid() const { return mCount != UINT32_MAX && mStartIndex != UINT32_MAX; }
+};
+
+struct PendingDeletion
+{
+	Allocation mAllocation;
+	uint64_t mFence;
 };
 
 class DescriptorHandle;
@@ -32,10 +43,13 @@ public:
 	uint32_t GetHeapSize() const { return mHeapCount; }
 	uint32_t GetDescriptorSize() const { return mDescriptorSize; }
 
-	// Allocations
+	/// Allocations
 	Allocation Allocate(uint32_t count);
 
 	DescriptorHandle GetHandle(uint32_t index);
+
+	void FreeDeferred(Allocation allocation, uint64_t fence);
+	void ProcessDeletions(uint64_t completedFence);
 
 private:
 	Microsoft::WRL::ComPtr<ID3D12DescriptorHeap> mHeap;
@@ -51,4 +65,27 @@ private:
 	D3D12_CPU_DESCRIPTOR_HANDLE mCpuStart;
 
 	D3D12_GPU_DESCRIPTOR_HANDLE mGpuStart;
+
+	/// Free List
+	/// NOTE look into intrusive lists for optimization
+
+	/// This data structure uses:
+	///     Key:   Block size, how many descriptors
+	///     Value: List of start indcies with that (key) size
+	///
+	/// When we free an Allocation {startIndex, count}, example we add to the free list
+	/// that we just free 10 descriptors at index 50, so mFreeList[10].push_back(50).
+	///
+	/// In other words, track how many slots in the descriptor heap we can reuse.
+	/// NOTE Slow, look into splitting or optimization if profile shows slowness.
+	std::map<uint32_t, std::vector<uint32_t>> mFreeList;
+
+	bool Free(Allocation allocation);
+
+	/// Pending Queue
+	std::queue<PendingDeletion> mPendingDeletion;
+
+	/// Logger
+	static std::shared_ptr<spdlog::logger> sLogger;
+	void InitLogger();
 };
